3a305f7f66f01626b3ace46c107d880ac1decea7
Angel Barros <albarros1973@gmail.com>
2021-08-09 23:12:47 +0100

    Create StaticPages Controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,29 @@
 class StaticPagesController < ApplicationController
-  def index
-  end
+  def index; end
+
+  def ten_reasons; end
+
+  def the_school; end
+
+  def infant_comuniy; end
+
+  def cdb; end
+
+  def primary_i; end
+
+  def primary_ii; end
+
+  def news; end
+
+  def secretariat; end
+
+  def contact; end
+
+  def visit; end
+
+  def location; end
+
+  def jobs; end
+
+  def language; end
 end
